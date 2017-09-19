86b30afc56a711d750f03ecf071b190ded89b3b2
timezstyle <timezstyle@gmail.com>
2017-09-19 15:17:00 +0800

    Update __main__.py

--- bzjira/__main__.py
+++ bzjira/__main__.py
@@ -29,7 +29,7 @@ def sync_bz_to_jira(bz, bz_id, jira, project_key, yes_all):
                                   issuetype={'name': 'Bug'},
                                   priority={'name': 'Critical' if bug.priority == 'P1' else 'Major'},
                                   customfield_10216=str(bug.bug_id),
-                                  customfield_10203=0,
+#                                   customfield_10203=0,
                                   customfield_10205=str(bug.long_desc))
         return issue
 
